0e6a9bf35113b4429172021782600ea96d47d820
Daniel Baker <dbaker731@gmail.com>
2016-10-05 13:00:23 -0500

    finishing the mini project

---
+++ js/app.js
@@ -0,0 +1,23 @@
+angular.module( 'miniRoute', [ 'ui.router' ] )
+  .config( function( $stateProvider, $urlRouterProvider ){
+
+    $stateProvider
+      .state( 'home', {
+        url: '/',
+        templateUrl: 'js/home/homeTmpl.html',
+      })
+      .state( 'settings', {
+        url: '/settings',
+        templateUrl: 'js/settings/settingsTmpl.html',
+      })
+      .state( 'products', {
+        url: '/products:id',
+        templateUrl: 'js/products/productsTmpl.html',
+        controller: 'productsCtrl'
+      });
+
+    $urlRouterProvider
+      .otherwise('/');
+
+
+  });
---
+++ js/products/productsCtrl.js
@@ -0,0 +1,11 @@
+angular.module( 'miniRoute' ).controller( 'productsCtrl', function( $scope, $stateParams, productsService){
+
+  if ( $stateParams.id === 'shoes' ) {
+    $scope.productData = productsService.shoeData;
+  } else if ( $stateParams.id === 'socks' ) {
+    $scope.productData = productsService.sockData;
+  }
+
+  $scope.test = productsService.sockData;
+
+});
